4664cb76aa98290b18d36f32fc60d0cefd9d26a7
viphoangvu1996 <viphoangvu0@gmail.com>
2017-04-23 16:57:58 +0700

    đừng quan tâm cái này

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,36 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\DSMonan;
 
 class HomeController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
-      $newest = DSMonan::orderBy('created_at', 'desc')
-                        ->take(9)
-                        ->get();
-      $thucuong = DSMonan::where('categorie', 3)
-                        ->orderBy('created_at', 'desc')
-                        ->take(8)
-                        ->get();
-      $trangmieng = DSMonan::where('categorie', 2)
-                        ->orderBy('created_at', 'desc')
-                        ->take(8)
-                        ->get();
-
-      return view('home', ['newest' => $newest, 'thucuong' => $thucuong, 'trangmieng' => $trangmieng, 'title' => 'Từ điển món ăn CookingA-Z']);
+        return view('home');
     }
-
-    public function inside($id)
-    {
-      return view();
-    }
-
-
 }
