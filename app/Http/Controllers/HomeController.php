c7d1380c82d8aaad0c8ad7cfb12a285ad871d834
mchan004 <thanhtulexuan@ThanhTus-MacBook-Pro.local>
2017-04-23 21:53:01 +0700

    Revert "đừng quan tâm cái này"
    This reverts commit 4664cb76aa98290b18d36f32fc60d0cefd9d26a7.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,26 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\DSMonan;
 
 class HomeController extends Controller
 {
     /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
-     * Show the application dashboard.
+     * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        return view('home');
+      $newest = DSMonan::orderBy('created_at', 'desc')
+                        ->take(9)
+                        ->get();
+      $thucuong = DSMonan::where('categorie', 3)
+                        ->orderBy('created_at', 'desc')
+                        ->take(8)
+                        ->get();
+      $trangmieng = DSMonan::where('categorie', 2)
+                        ->orderBy('created_at', 'desc')
+                        ->take(8)
+                        ->get();
+
+      return view('home', ['newest' => $newest, 'thucuong' => $thucuong, 'trangmieng' => $trangmieng, 'title' => 'Từ điển món ăn CookingA-Z']);
     }
+
+    public function inside($id)
+    {
+      return view();
+    }
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -10,86 +10,11 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-use Illuminate\Http\Request;
+
 Route::get('/', 'HomeController@index');
 
-Route::get('/{id}/{title}', 'HomeController@inside')->where(['id' => '[0-9]+']);
+Route::get('/{id}/{title}', 'HomeController@inside')->where(['id' => '[0-9]+');
 
 Route::get('/outside', function () {
     return view('outside');
 });
-
-Route::get('/monan', function () {
-  $monan=App\DSMonan::All();
-  $monanchitiet=new App\DSMonan;
-  return view('quanly')->with('monan',$monan)->with('monanchitiet',$monanchitiet);
-});
-
-Route::get('/monan/{id}', function ($id) {
-  $monan=App\DSMonan::All();
-  $monanchitiet=App\DSMonan::where('id',$id)->get();
-    foreach ($monanchitiet as $key) {
-      return view('quanly')->with('monan',$monan)->with('monanchitiet',$key);
-    }
-});
-
-Route::post('/capnhatmonan/{id}', function (Request $request,$id) {
-    $monan=App\DSMonan::find($id);
-
-    $monan->tenMonan=$request['tenMonan'];
-    $monan->categorie=$request['categorie'];
-    $monan->level=$request['level'];
-    $monan->nhaTaitro=$request['nhataitro'];
-    $monan->gioithieu=$request['gioithieu'];
-    $monan->hinhMinhhoa=$request['hinhminhhoa'];
-    $monan->origin=$request['origin'];
-    $monan->thoigian=$request['thoigian'];
-    $monan->dokho=$request['dokho'];
-    $monan->video=$request['video'];
-    $monan->huongdan=$request['huongdan'];
-    $monan->nguon=$request['nguon'];
-    $monan->hits=$request['hits'];
-    $monan->publish=$request['publish'];
-
-    $monan->save();
-    $ketqua="Cập nhật thành công";
-    return $ketqua;
-});
-
-Route::get('/them', function () {
-    $monan=new App\DSMonan;
-    $monan->tenMonan="0";
-    $monan->categorie="1";
-    $monan->level="0";
-    $monan->nhaTaitro="0";
-    $monan->gioithieu="0";
-    $monan->hinhMinhhoa="0";
-    $monan->origin="0";
-    $monan->thoigian="0";
-    $monan->dokho="0";
-    $monan->video="0";
-    $monan->huongdan="0";
-    $monan->nguon="0";
-    $monan->hits="0";
-    $monan->publish="0";
-    $monan->createby="1";
-
-    $monan->save();
-
-    $monan=App\DSMonan::All();
-    $monanchitiet=new App\DSMonan;
-    return view('quanly')->with('monan',$monan)->with('monanchitiet',$monanchitiet);
-});
-
-Route::get('/xoa/{id}', function ($id) {
-    $monan=App\DSMonan::find($id);
-    $monan->delete();
-});
-
-Route::auth();
-
-Route::get('/home', 'HomeController@index');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index');
